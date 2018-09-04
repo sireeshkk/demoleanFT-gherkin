618810074a0af745cdc7909d03c8465e8678591c
demo <demo@MF.COM>
2018-09-04 10:33:31 +0000

    Changes updated

---
+++ src/main/java/net/mf/LeanFtStepDefinitions.java
@@ -0,0 +1,124 @@
+package net.mf;
+
+import java.io.IOException;
+import com.hp.lft.report.ReportException;
+import com.hp.lft.report.Reporter;
+import com.hp.lft.report.Status;
+import com.hp.lft.sdk.GeneralLeanFtException;
+import com.hp.lft.sdk.*;
+import com.hp.lft.verifications.*;
+import static org.junit.Assert.*;
+
+import cucumber.api.PendingException;
+import cucumber.api.Scenario;
+import cucumber.api.java.After;
+import cucumber.api.java.Before;
+import cucumber.api.java.en.Given;
+import cucumber.api.java.en.Then;
+import cucumber.api.java.en.When;
+import com.hp.lft.sdk.web.*;
+import org.junit.Assert;
+
+public class LeanFtStepDefinitions
+
+{
+    Browser browser;
+    AOSModel aosModel;
+
+    @Before
+        public void beforeVerifyScenario(Scenario scenario) throws IOException, GeneralLeanFtException, ReportException {
+        //the following will start a new test node in the report
+       browser = BrowserFactory.launch(BrowserType.CHROME);
+        aosModel = new AOSModel(browser);
+      }
+
+    @After
+    public void afterVerifyScenario() throws InterruptedException, ReportException, GeneralLeanFtException {
+        browser.close();
+    }
+
+
+    @Given("^I am in the site$")
+    public void i_am_in_the_site() throws Throwable {
+        // Write code here that turns the phrase above into concrete actions
+
+        //Browser browser = BrowserFactory.launch(BrowserType.CHROME);
+
+        browser.navigate("http://www.advantageonlineshopping.com/");
+
+    }
+
+    @Given("^I select the Mice category$")
+    public void i_select_the_Mice_category() throws Throwable {
+        // Write code here that turns the phrase above into concrete actions
+
+/* The below code was left so you can easily see the difference between
+           descriptive programming and using the Application Models
+        ---------------------------------------------------------------------------------------------------------
+        WebElement miceCategory = browser.describe(WebElement.class, new WebElementDescription.Builder()
+                .className("micesImg categoryCell").tagName("DIV").build());
+        miceCategory.highlight();
+        miceCategory.click();
+        */
+
+        aosModel.MICE_tile().highlight();
+        aosModel.MICE_tile().click();
+
+       // throw new PendingException();
+    }
+
+    @When("^I filter by \"([^\"]*)\" color$")
+    public void i_filter_by_color(String strSelectColor) throws Throwable {
+
+        browser.describe(WebElement.class, new WebElementDescription.Builder()
+                .tagName("H4").innerText("COLOR ").build()).click();
+
+        String strHexColor = null;
+        switch (strSelectColor.toLowerCase()) {
+            case "white":
+                strHexColor = "FFFFFF";
+                break;
+            case "black":
+                strHexColor = "414141";
+                break;
+            case "blue":
+                strHexColor = "3683D1";
+                break;
+            case "gray":
+                strHexColor = "C3C3C3";
+                break;
+            case "purple":
+                strHexColor = "545195";
+                break;
+            case "red":
+                strHexColor = "DD3A5B";
+                break;
+            case "yellow":
+                strHexColor = "FCC23D";
+                break;
+        }
+
+            browser.describe(WebElement.class, new WebElementDescription.Builder()
+                    .className("productColor ").id("productsColors"+strHexColor).tagName("A").innerText("").build()).click();
+
+    }
+
+    @Then("^the mouse price is \"([^\"]*)\"$")
+    public void the_mouse_price_is(String dblPrice) throws Throwable {
+        // Write code here that turns the phrase above into concrete actions
+
+        Thread.sleep(2000);
+        String strWebPrice = browser.describe(WebElement.class, new CSSDescription(".category-type-products .cell:nth-child(2) ul .ng-scope:nth-child(1) p:nth-child(5) .productPrice")).getInnerText();
+
+        //Verify.areEqual(Double.toString(dblPrice),strWebPrice.trim());
+        //This to generate the html report
+        Verify.areEqual(dblPrice, strWebPrice.trim());
+
+        //Assert.assertTrue(dblPrice.trim().equals(strWebPrice.trim()));
+        //This to have junit report correct for reporting to Octane
+        if (Reporter.getStatus() == Status.Failed){
+            Assert.fail();
+        }
+        // throw new PendingException();
+    }
+}
---
+++ src/main/java/net/mf/LeanFtTest.java
@@ -0,0 +1,40 @@
+package net.mf;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import cucumber.api.CucumberOptions;
+import cucumber.api.junit.Cucumber;
+import com.hp.lft.sdk.*;
+import com.hp.lft.verifications.*;
+
+import unittesting.*;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(plugin = {"com.hp.lft.report.CucumberReporter"},
+                 features = "src/main/java/net/mf")
+public class LeanFtTest extends UnitTestClassBase {
+
+    public LeanFtTest() {
+        //Change this constructor to private if you supply your own public constructor
+    }
+
+    @BeforeClass
+    public static void setUpBeforeClass() throws Exception {
+        instance = new LeanFtTest();
+        globalSetup(LeanFtTest.class);
+    }
+
+    @AfterClass
+    public static void tearDownAfterClass() throws Exception {
+        globalTearDown();
+    }
+
+    @Test
+    public void test1() throws GeneralLeanFtException {
+    }
+
+}
